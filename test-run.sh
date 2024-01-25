0225ac816ece2d7e64c15381ea68df6e25388abd
gxshub <guoxin2009@gmail.com>
2024-01-25 22:29:25 +1100

    update test run

---
+++ test-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/build/mopmc --help
+#./build/mopmc -M examples/dive_and_rise/dive_and_rise.nm -P examples/dive_and_rise/dive_and_rise_prop_100.props -Q convex -L mse -I si-gd
+./build/mopmc -M examples/multiobj_scheduler05.nm -P examples/multiobj_scheduler05.pctl -Q achievability
