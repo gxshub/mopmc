94e7babcafd617284cb4f25e9ee677052bae670d
gxshub <61297328+gxshub@users.noreply.github.com>
2024-02-10 09:34:01 +1100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cd ./build ; cmake .. -Wno-dev -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+export ARCH=$(dpkg --print-architecture)
+if [![ $ARCH == "amd64" ]] ; then
+  tar -xvzf ./misc/lp_solve_5.5.2.11_source.tar.gz
+  sh ./misc/lp_solve_5.5.2.11_source/lp_solve_5.5/lpsolve55/ccc
+  mv ./misc/lp_solve_5.5.2.11_source/lp_solve_5.5/lpsolve55/liblpsolve55.* ./lpSolve/
+cd ./build ; cmake .. -Wno-dev -DCMAKE_EXPORT_COMPILE_COM
